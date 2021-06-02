b6d93517c0f3592566d3d69546fb543ff7631d81
KseniaFalcoz <kseniafalcoz@gmail.com>
2021-06-02 11:38:26 +0200

    exercices TDD Jasmine

---
+++ spec/questions.spec.js
@@ -0,0 +1,76 @@
+describe('Les chaines de caractères : ', function () {   //on décrit la fonctionnalité
+// "it" indique le comportement attendu
+    it('Donner la taille d\'une chaine de caractères', function () {
+      // on crée un exemple
+        var result = tailleString('Ce texte à une certaine taille');
+        // on donne le reésultat attendu
+        expect(result).toEqual(30);
+    });
+
+// 2
+  it('Remplacer le premier e d\'une chaine par un espace', function () {
+    var result = replaceString('Ce texte a une certaine taille');
+    expect(result).toEqual('C  texte a une certaine taille');
+  });
+
+// 3
+  it('Concatener deux chaines de caractères', function () {
+    var result = concatString('abcdef', '12345');
+    expect(result).toEqual('abcdef12345');
+    });
+
+// 4
+  it('Afficher le cinquième caractère d\'une chaine', function () {
+    var result = charAtString5('Ce texte a une certaine taille');
+    expect(result).toEqual('x');
+  });
+
+// 5
+  it('Afficher les 9 premiers caractères d\'une chaîne de caractère', function () {
+    var result = characters9('Ce texte a une certaine taille');
+    expect(result).toEqual('Ce texte ');
+  });
+  
+// 6
+  it('Mettre en majuscule la chaine', function () {
+    var result = upperCaseString('abcdef');
+    expect(result).toEqual('ABCDEF');
+  });
+
+// 7
+  it('Mettre en minuscule la chaine', function () {
+    var result = lowerCaseString('ABCDEF');
+    expect(result).toEqual('abcdef');
+  });
+
+// 8
+  it('Supprimer les espaces avant et après la chaine', function () {
+    var result = deleteSpace(' abcdef ');
+    expect(result).toEqual('abcdef');
+  });
+
+// 9
+  it('Afficher true si le parametre d\'entrée de la fonction est de type string', function () {
+    var result = typeString('Ce texte a une certaine taille');
+    expect(result).toEqual(true);
+  });
+
+// 10
+  it('Afficher l\'extension du fichier', function () {
+    var result = extension('001.jpg');
+    expect(result).toEqual('jpg');
+  });
+
+// 11
+  it('Compter le nombre d\'espace dans la chaine', function () {
+    var result = numberSpace('Ce texte a une certaine taille');
+    expect(result).toEqual(5);
+  });
+
+// 12
+  it('Inverser une chaine de caractères', function () {
+    var result = reverseString('12345678');
+    expect(result).toEqual('87654321');
+  });
+
+});
---
+++ src/questions.js
@@ -0,0 +1,65 @@
+/*
+ * Exercice sur les chaines de caractères.
+ * Trouvez la façon de faire la plus optimal.
+ * Il peut y avoir plusieur façon de faire.
+ */
+let tailleString = function(string){
+    return string.length;
+}
+
+// 2
+let replaceString = function(string){
+    return string.replace('e', ' ');
+}
+
+// 3
+let concatString = function(string1, string2){
+    return string1 + string2;
+}
+
+// 4
+let charAtString5 = function(string){
+    return string.charAt(5);
+}
+
+// 5
+let characters9 = function(string){
+    return string.substring(0, 9);
+}
+
+// 6
+let upperCaseString = function(string){
+    return string.toUpperCase();
+}
+
+// 7
+let lowerCaseString = function(string){
+    return string.toLowerCase();
+}
+
+// 8
+let deleteSpace = function(string){
+    return string.trim();
+}
+
+// 9
+let typeString = function(string){
+    if (typeof "" === 'string'){
+        return true;
+    }
+}
+
+// 10
+let extension = function(string){
+    return string.split('.').pop();
+}
+
+// 11
+let numberSpace = function(string){
+    return string.split(' ').length - 1;
+}
+
+// 12
+let reverseString = function(string){
+    return string.split('').reverse().join('');
+}
